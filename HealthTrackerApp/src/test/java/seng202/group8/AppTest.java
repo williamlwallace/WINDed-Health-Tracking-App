d47c27b14ee6151900878cc3c00c61dbc73d3ac7
Clarke <cmc280@uclive.ac.nz>
2018-08-08 13:09:29 +1200

    Test - Clarke

--- HealthTrackerApp/src/test/java/seng202/group8/AppTest.java
+++ HealthTrackerApp/src/test/java/seng202/group8/AppTest.java
@@ -8,32 +8,28 @@ import junit.framework.TestSuite;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 
     /**
@@ -48,6 +44,13 @@ public class AppTest
      * Joel Ridden
      */
     public void JoelRiddenTest1() {
+        assertTrue(true);
+    }
+
+    /**
+     * Clarke Mcfadzien
+     */
+    public void clarkeTest() {
         assertTrue(true);
     }
 }
