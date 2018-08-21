036292ffbefe7a6229d8fdff8101c70fcdbf7049
Jack Orchard <jco165@uclive.ac.nz>
2018-08-22 11:44:51 +1200

    Merge branch 'master' of /home/cosc/student/jco165/Documents/SENG202_Project/SENG202Team8 with conflicts.

--- HealthTrackerApp/src/main/java/seng202/group8/User/User.java
+++ HealthTrackerApp/src/main/java/seng202/group8/User/User.java
@@ -1,4 +1,52 @@
 package seng202.group8.User;
 
+import seng202.group8.User.User_Stats.UserStats;
+
+/**
+ * User is the class which stores the User data for a user of the Winded App
+ * @author jco165
+ */
 public class User {
+
+    private String name;
+    private Integer age;
+    private Double weight;
+    private Double height;
+    //private ArrayList<ActivityType> favouriteActivities;
+    private BMIType BMI;
+    private UserStats userStats;
+    private StressLevelType stressLevel;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public BMIType getBMI() {
+        return BMI;
+    }
+
+    public UserStats getUserStats() {
+        return userStats;
+    }
+
+    public StressLevelType getStressLevel() {
+        return stressLevel;
+    }
+
+
 }
