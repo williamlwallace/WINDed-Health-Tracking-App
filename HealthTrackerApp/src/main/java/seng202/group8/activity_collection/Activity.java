fa6d033b7d0b551556a8264d688f844046f5affa
Lorenzo <faso.9603@gmail.com>
2018-08-22 11:26:30 +1200

    Added an activity_collection packet and initial modules to gateher user activities

---
+++ HealthTrackerApp/src/main/java/seng202/group8/activity_collection/Activity.java
@@ -0,0 +1,9 @@
+package seng202.group8.activity_collection;
+
+public class Activity {
+    int mockVal;
+
+    public int getMockVal() {
+        return mockVal;
+    }
+}
---
+++ HealthTrackerApp/src/main/java/seng202/group8/activity_collection/ActivityList.java
@@ -0,0 +1,59 @@
+package seng202.group8.activity_collection;
+
+import java.util.ArrayList;
+
+public class ActivityList {
+
+
+    private ArrayList<Activity> activityList;
+
+
+    /**
+     * Structure containing Activity object, offers storage, sort and retrieval of this objects.
+     */
+    public ActivityList() {
+        activityList= new ArrayList<Activity>();
+    }
+
+
+    /**
+     * @param index index of Activity object to retrieve
+     * @return Activity object at specified index
+     */
+    public Activity getActivity(int index) {
+        return activityList.get(index);
+    }
+
+    /**
+     *
+     * @param activity a new Activity object to add to the activityList attribute
+     * @return a boolean value
+     */
+    public boolean insertActivity(Activity activity) {
+        int initialSize = activityList.size();
+        for (int i = 0; i < activityList.size(); i++) {
+            if (activity.mockVal < activityList.get(i).mockVal) {
+                activityList.add(i, activity);
+            }
+        }
+        activityList.add(activity);
+        int finalSize = activityList.size();
+
+        return (finalSize == (initialSize + 1));
+    }
+
+    /**
+     * @return the ArrayList<Activity> attribute
+     */
+    public ArrayList<Activity> getActivityList() {
+        return activityList;
+    }
+
+    /**
+     * @param activityList an ArrayList<Activity> object
+     */
+    public void setActivityList(ArrayList<Activity> activityList) {
+        this.activityList = activityList;
+    }
+
+}
