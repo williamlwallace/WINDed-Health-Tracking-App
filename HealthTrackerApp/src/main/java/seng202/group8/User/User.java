b1d4970ed70bb445f1b24687cb6900cccc262865
Jack Orchard <jco165@uclive.ac.nz>
2018-08-22 11:59:12 +1200

    Updated User Package classes to match the UML

--- HealthTrackerApp/src/main/java/seng202/group8/User/User.java
+++ HealthTrackerApp/src/main/java/seng202/group8/User/User.java
@@ -17,33 +17,74 @@ public class User {
     private UserStats userStats;
     private StressLevelType stressLevel;
 
+    /**
+     * Construct a new user
+     * @param name
+     * @param age
+     * @param weight
+     * @param height
+     */
+    public User(String name, Integer age, Double weight, Double height) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        //setBMI()
+    }
 
-
-
+    /**
+     *
+     * @return The name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return The age of the user
+     */
     public Integer getAge() {
         return age;
     }
 
+    /**
+     *
+     * @return The weight of the user (kg)
+     */
     public Double getWeight() {
         return weight;
     }
 
+
+    /**
+     *
+     * @return The height of the user (cm)
+     */
     public Double getHeight() {
         return height;
     }
 
+    /**
+     *
+     * @return The BMI type of the user
+     */
     public BMIType getBMI() {
         return BMI;
     }
 
+    /**
+     *
+     * @return The users stats from activities
+     */
     public UserStats getUserStats() {
         return userStats;
     }
 
+    /**
+     *
+     * @return the Stress Level Type of the user
+     */
     public StressLevelType getStressLevel() {
         return stressLevel;
     }
