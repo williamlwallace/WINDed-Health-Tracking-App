2b3a59dbf2e566136f699da301479398933acbe3
Joel Ridden <jnr26@uclive.ac.nz>
2018-08-29 17:19:39 +1200

    Creating a new package Satistics_Service, creating a new class to hold the x and y axis objects

---
+++ HealthTrackerApp/src/main/java/seng202/group8/Services/Statistics_Service/GraphXY.java
@@ -0,0 +1,17 @@
+package seng202.group8.Services.Statistics_Service;
+
+import java.util.ArrayList;
+
+public class GraphXY {
+
+    public ArrayList<Double> getXAxis() {
+        return XAxis;
+    }
+
+    public ArrayList<Double> getYAxis() {
+        return YAxis;
+    }
+
+    public ArrayList<Double> YAxis;
+    public ArrayList<Double> XAxis;
+}
---
+++ HealthTrackerApp/src/main/java/seng202/group8/Services/Statistics_Service/StatisticsService.java
@@ -0,0 +1,71 @@
+package seng202.group8.Services.Statistics_Service;
+
+import java.util.ArrayList;
+
+public class StatisticsService {
+
+    public Integer getAverageHeartRate() {
+        return averageHeartRate;
+    }
+
+    public void setAverageHeartRate(Integer averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+    }
+
+    public Double getKmRunWeek() {
+        return kmRunWeek;
+    }
+
+    public void setKmRunWeek(Double kmRunWeek) {
+        this.kmRunWeek = kmRunWeek;
+    }
+
+    public Double getmSwamWeek() {
+        return mSwamWeek;
+    }
+
+    public void setmSwamWeek(Double mSwamWeek) {
+        this.mSwamWeek = mSwamWeek;
+    }
+
+    public Double getKmBikedWeek() {
+        return kmBikedWeek;
+    }
+
+    public void setKmBikedWeek(Double kmBikedWeek) {
+        this.kmBikedWeek = kmBikedWeek;
+    }
+
+    public String getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(String healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public Double getCaloriesBurnedWeek() {
+        return caloriesBurnedWeek;
+    }
+
+    public void setCaloriesBurnedWeek(Double caloriesBurnedWeek) {
+        this.caloriesBurnedWeek = caloriesBurnedWeek;
+    }
+    public Double getWeightLossWeek() {
+        return weightLossWeek;
+    }
+
+    public void setWeightLossWeek(Double weightLossWeek) {
+        this.weightLossWeek = weightLossWeek;
+    }
+
+
+    public Integer averageHeartRate;
+    public Double kmRunWeek;
+    public Double mSwamWeek;
+    public Double kmBikedWeek;
+    public String healthStatus;
+    public Double caloriesBurnedWeek;
+    public Double weightLossWeek;
+
+}
