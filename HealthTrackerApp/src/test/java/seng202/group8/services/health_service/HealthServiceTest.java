3ffd37bea3c83cd005a8086e1ec98b3aa9f1a4d3
Lorenzo Fasano <faso.9603@gmail.com>
2018-08-31 22:02:13 -0700

    Finished testing core behaviour of HealthService class

--- HealthTrackerApp/src/test/java/seng202/group8/services/health_service/HealthServiceTest.java
+++ HealthTrackerApp/src/test/java/seng202/group8/services/health_service/HealthServiceTest.java
@@ -26,10 +26,6 @@ public class HealthServiceTest {
     public void setUp() throws Exception {
         user = new User("Lorenzo", 22, 83.0, 183.0);
         healthService = new HealthService(user);
-//        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
-//        heartRateList.add(130);
-//        heartRateList.add(21);
-//        runData = new RunData("Run forrest run!", DataType.RUN, new ArrayList<Double>(), heartRateList);
         activityList = new ActivityList("Bad forrest gump imitation");
     }
 
@@ -43,21 +39,134 @@ public class HealthServiceTest {
     }
 
     @Test
-    public void checkIsTachicardicTrue() {
+    public void checkIsTachicardicHeavyWorkTrue() {
         ArrayList<Integer> heartRateList = new ArrayList<Integer>();
-        heartRateList.add(330);
+        heartRateList.add(169);//Minimum for tachicardia (220 - 22) * 0.85 = 168.7
         heartRateList.add(21);
         runData = new RunData("Run forrest run!", DataType.RUN, new ArrayList<Double>(), heartRateList);
         user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, runData);
+        user.getUserActivities().insertActivityInGivenList(0, runData);
         user.getUserActivities().insertActivityInGivenList(0, runData);
         assertTrue(healthService.isTachicardic());
     }
 
     @Test
-    public void checkIsBradicardic() {
+    public void checkIsTachicardicHeavyWorkFalse() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(169);
+        heartRateList.add(21);
+        runData = new RunData("Run forrest run!", DataType.RUN, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, runData);
+        user.getUserActivities().insertActivityInGivenList(0, runData);
+        assertFalse(healthService.isTachicardic());
     }
 
     @Test
-    public void checkIsAtCardiovascularRisk() {
+    public void checkIsTachicardicLightWorkTrue() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(21);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertTrue(healthService.isTachicardic());
     }
+
+    @Test
+    public void checkIsTachicardicMixedtWorkTrue() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(169);
+        heartRateList.add(21);
+        runData = new RunData("Run forrest run!", DataType.RUN, new ArrayList<Double>(), heartRateList);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, runData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertTrue(healthService.isTachicardic());
+    }
+
+
+    @Test
+    public void checkIsTachicardicLightWorkFalse1() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(99);
+        heartRateList.add(21);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertFalse(healthService.isTachicardic());
+    }
+
+    @Test
+    public void checkIsTachicardicLightWorkFalse2() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(21);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertFalse(healthService.isTachicardic());
+    }
+
+
+    @Test
+    public void checkIsBradicardicTrue() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(59);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertTrue(healthService.isBradicardic());
+    }
+
+    @Test
+    public void checkIsBradicardicFalse() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(59);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+
+        assertFalse(healthService.isBradicardic());
+    }
+
+
+    @Test
+    public void checkIsAtCardiovascularRiskTrue() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(59);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertTrue(healthService.isAtCardiovasMortalityRisk());
+    }
+
+    @Test
+    public void checkIsAtCardiovascularRiskFalse() {
+        ArrayList<Integer> heartRateList = new ArrayList<Integer>();
+        heartRateList.add(101);
+        heartRateList.add(59);
+        walkData = new WalkData("Walk forrest walk!", DataType.WALK, new ArrayList<Double>(), heartRateList);
+        user.getUserActivities().insertActivityList(activityList);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        user.getUserActivities().insertActivityInGivenList(0, walkData);
+        assertFalse(healthService.isAtCardiovasMortalityRisk());
+    }
+
 }
