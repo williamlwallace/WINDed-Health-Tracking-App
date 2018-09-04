10381dac21fddebbb3c12f2c340067a72c9389a1
wwa52 <wwa52@uclive.ac.nz>
2018-09-04 19:08:16 +1200

    updated GUI files

--- HealthTrackerApp/src/main/java/seng202/group8/gui/GUIController.java
+++ HealthTrackerApp/src/main/java/seng202/group8/gui/GUIController.java
@@ -1,9 +1,15 @@
 package seng202.group8.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ProgressIndicator;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class GUIController {
 
@@ -14,5 +20,16 @@ public class GUIController {
     private void handleButtonAction(ActionEvent event) {
 
         progressIndicator.setProgress(-1.0f);
+    }
+
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene homeScene = new Scene(root);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(homeScene);
+        window.show();
+
     }
 }
