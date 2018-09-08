5556051f3bfa14dc55e98228635759d4c699707c
Samverd <sgv15@uclive.ac.nz>
2018-09-08 16:18:30 +1200

    Added classes to handle initial data entry, still a work in progress

---
+++ HealthTrackerApp/src/main/java/seng202/group8/gui/user_info_gui/GetUserInfoController.java
@@ -0,0 +1,10 @@
+package seng202.group8.gui.user_info_gui;
+
+import javafx.event.ActionEvent;
+
+public class GetUserInfoController {
+
+    public void enterDetails(ActionEvent event) {
+        System.out.printf("hello");
+    }
+}
