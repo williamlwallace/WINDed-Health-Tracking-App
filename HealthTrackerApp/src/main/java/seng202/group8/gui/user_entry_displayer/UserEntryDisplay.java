6f97abb9499258cb1b6a7fa9025c9206185efcb7
Jack Orchard <jco165@uclive.ac.nz>
2018-09-08 17:23:24 +1200

    Added the GUI display components for the User Entry

---
+++ HealthTrackerApp/src/main/java/seng202/group8/gui/user_entry_displayer/UserEntryDisplay.java
@@ -0,0 +1,16 @@
+package seng202.group8.gui.user_entry_displayer;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class UserEntryDisplay extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+}
