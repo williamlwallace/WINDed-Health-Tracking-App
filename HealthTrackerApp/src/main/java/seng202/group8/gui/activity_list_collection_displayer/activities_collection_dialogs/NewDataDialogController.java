66f53e4be350e8d12dde91a7b5baa5f2d74c126a
Lorenzo <faso.9603@gmail.com>
2018-09-20 14:43:40 +1200

    Started adding the logic into creating a new Data object given the user input.

--- HealthTrackerApp/src/main/java/seng202/group8/gui/activity_list_collection_displayer/activities_collection_dialogs/NewDataDialogController.java
+++ HealthTrackerApp/src/main/java/seng202/group8/gui/activity_list_collection_displayer/activities_collection_dialogs/NewDataDialogController.java
@@ -1,6 +1,9 @@
 package seng202.group8.gui.activity_list_collection_displayer.activities_collection_dialogs;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXToggleButton;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,6 +14,7 @@ import javafx.stage.Stage;
 import seng202.group8.user.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NewDataDialogController {
 
@@ -40,6 +44,12 @@ public class NewDataDialogController {
     private JFXButton createDataButton;
 
     @FXML
+    private JFXToggleButton newActivityListToggle;
+
+    @FXML
+    private TextField newActivityListName;
+
+    @FXML
     public void initialize() {
         ArrayList<String> activitiesChoice = new ArrayList<>();
         activitiesChoice.add("Walk");
@@ -51,9 +61,61 @@ public class NewDataDialogController {
         activitiesChoice.add("Water Sport");
         ObservableList<String> observableList = FXCollections.observableList(activitiesChoice);
         activitiesChoiceBox.setItems(observableList);
+        activitiesChoiceBox.setValue("Walk");
 //        activitiesChoiceBox.se
 
+        distanceCoveredTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d")) {
+                    distanceCoveredTextField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        ArrayList<Integer> hours = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            hours.add(i);
+        }
+        ObservableList<Integer> integerObservableList = FXCollections.observableList(hours);
+        fromTime.setItems(integerObservableList);
+        toTime.setItems(integerObservableList);
+        fromTime.setValue(hours.get(0));
+        toTime.setValue(hours.get(0));
+
+        newActivityListName.setVisible(false);
+
     }
+
+
+    public void togglListener() {
+        if (newActivityListToggle.isSelected()) {
+            newActivityListName.setVisible(true);
+        } else {
+            newActivityListName.setVisible(false);
+        }
+    }
+
+    public void createActivity() {
+
+        if (allFieldsAreCorrect()) {
+            if (newActivityListToggle.isSelected()) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    private boolean allFieldsAreCorrect() {
+        boolean descrNoNull = !descriptionTextField.getText().equals("");
+        return true;
+    }
+
+
+
 
 
 
