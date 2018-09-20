9e8f5ca5ad9357abc8729c6cc912e9e5c400a50d
Clarke Mcfadzien <cmc280@uclive.ac.nz>
2018-09-21 11:16:06 +1200

    Changed Bike, Walk, and Run data for now, to reduce chance of errors before deliverable 2. These have a more detailed implementation, but I have removed it for now and may add it back in for deliverable 3. (I had written the detailed implementation before I found the simple implementation I am currently using. Or else I would have used the simple implementation from the start.)
    Its simply not a priority for the detailed implementation
    to be working correctly at this stage, because the simple implementation
    is still good enough.

--- HealthTrackerApp/src/main/java/seng202/group8/data_entries/BikeData.java
+++ HealthTrackerApp/src/main/java/seng202/group8/data_entries/BikeData.java
@@ -10,8 +10,8 @@ public class BikeData extends AssistedSportsData{
     public BikeData(String newTitle, DataType dataType, ArrayList<LocalDateTime> newDateTimes, ArrayList<CoordinateData> newCoordinatesList, ArrayList<Integer> newHeartRateList, User theCurrentUser) {
         super(newTitle, dataType, newDateTimes, newCoordinatesList, newHeartRateList, theCurrentUser);
 
-        double consumedCalories = 0.0;
-        this.consumedCalories = getConsumedCalories();
+        //double consumedCalories = 0.0;
+        //this.consumedCalories = getConsumedCalories();
     }
 
 
@@ -19,35 +19,60 @@ public class BikeData extends AssistedSportsData{
 //        super(newTitle, activityType, newCoordinatesList, newHeartRateList);
 //    }
 
-
     public double getConsumedCalories() {
-        /** Calculates the calories burned whilst biking based on a MET intensity value, which is determined from the
-         * heartrate, and incline.
-         */
 
-        Double[] metValuesArray = {3.5, 5.8, 6.8, 8.0, 10.0};
-        Double[] speedDivisionsArray = {5.5, 9.4, 11.9, 13.9, 15.9};
+        double calories = calculateCaloriesFromUserStatsAndHeartRateAndTime();
 
-        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
-        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
-
-
-        Double calorieTotal = calculateCaloriesFromStats(metValues, speedDivisions);
-
-        return calorieTotal;
+        return calories;
+        //if (this.getHeartRateList() == 0);
     }
 
     public ArrayList<Double> getConsumedCaloriesBetweenPoints() {
+        ArrayList<Double> caloriesList = new ArrayList<Double>();
 
-        Double[] metValuesArray = {3.5, 5.8, 6.8, 8.0, 10.0};
-        Double[] speedDivisionsArray = {5.5, 9.4, 11.9, 13.9, 15.9};
+        caloriesList = calculateCaloriesBurnedBetweenPointsFromUserStatsAndHeartRateAndTime();
 
-        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
-        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
-        ArrayList<Double> caloriesList;
-
-        caloriesList = calculateCaloriesBurnedBetweenPointsFromStats(metValues, speedDivisions);
         return caloriesList;
     }
+    /**
+     * Below is a calorie calculation that should be more accurate for users that have not updated their information
+     * (such as weight) for a long time. However, it does not work properly. It may be fixed before deliverable three,
+     * to improve usability,
+     * but the existing calculation is valid as long as the user information is correct, so this may have to take a lower
+     * priority (its like an extra feature).
+     *
+     * This calculation was designed first, but one that was easier to implement was found near the end. So the more
+     * easily implemented one will be used for now.
+     */
+
+//    public double getConsumedCalories() {
+//        /** Calculates the calories burned whilst biking based on a MET intensity value, which is determined from the
+//         * heartrate, and incline.
+//         */
+//
+//        Double[] metValuesArray = {3.5, 5.8, 6.8, 8.0, 10.0};
+//        Double[] speedDivisionsArray = {5.5, 9.4, 11.9, 13.9, 15.9};
+//
+//        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
+//        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
+//
+//
+//        Double calorieTotal = calculateCaloriesFromStats(metValues, speedDivisions);
+//
+//        return calorieTotal;
+//    }
+//
+//    public ArrayList<Double> getConsumedCaloriesBetweenPoints() {
+//
+//        Double[] metValuesArray = {3.5, 5.8, 6.8, 8.0, 10.0};
+//        Double[] speedDivisionsArray = {5.5, 9.4, 11.9, 13.9, 15.9};
+//
+//        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
+//        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
+//        ArrayList<Double> caloriesList;
+//
+//        caloriesList = calculateCaloriesBurnedBetweenPointsFromStats(metValues, speedDivisions);
+//        return caloriesList;
+//    }
 
 }
--- HealthTrackerApp/src/main/java/seng202/group8/data_entries/WalkData.java
+++ HealthTrackerApp/src/main/java/seng202/group8/data_entries/WalkData.java
@@ -11,39 +11,58 @@ public class WalkData extends NotAssistedSportsData {
         super(newTitle, dataType, newDateTimes, newCoordinatesList, newHeartRateList, theCurrentUser);
     }
 
+
     public double getConsumedCalories() {
 
-        Double[] metValuesArray = {2.8, 3.0, 3.5, 4.3, 5.0, 7.0, 8.3};
-        Double[] speedDivisionsArray = {2.0, 2.5, 3.2, 3.5, 4.0, 4.5, 5.0};
+        double calories = calculateCaloriesFromUserStatsAndHeartRateAndTime();
 
-        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
-        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
-
-
-        Double calorieTotal = calculateCaloriesFromStats(metValues, speedDivisions);
-
-        return calorieTotal;
+        return calories;
         //if (this.getHeartRateList() == 0);
     }
 
     public ArrayList<Double> getConsumedCaloriesBetweenPoints() {
+        ArrayList<Double> caloriesList = new ArrayList<Double>();
 
-        Double[] metValuesArray = {2.8, 3.0, 3.5, 4.3, 5.0, 7.0, 8.3};
-        Double[] speedDivisionsArray = {2.0, 2.5, 3.2, 3.5, 4.0, 4.5, 5.0};
+        caloriesList = calculateCaloriesBurnedBetweenPointsFromUserStatsAndHeartRateAndTime();
 
-        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
-        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
-        ArrayList<Double> caloriesList = new ArrayList<>();
-
-        caloriesList = calculateCaloriesBurnedBetweenPointsFromStats(metValues, speedDivisions);
         return caloriesList;
     }
-}
-//    public WalkData(String newTitle, DataType activityType, ArrayList<Double> newCoordinatesList, ArrayList<Integer> newHeartRateList) {
-//        super(newTitle, activityType, newCoordinatesList, newHeartRateList);
+    /**
+     * Below is a calorie calculation that should be more accurate for users that have not updated their information
+     * (such as weight) for a long time. However, it does not work properly. It may be fixed before deliverable three,
+     * to improve usability,
+     * but the existing calculation is valid as long as the user information is correct, so this may have to take a lower
+     * priority (its like an extra feature).
+     *
+     * This calculation was designed first, but one that was easier to implement was found near the end. So the more
+     * easily implemented one will be used for now.
+     */
+//    public double getConsumedCalories() {
+//
+//        Double[] metValuesArray = {2.8, 3.0, 3.5, 4.3, 5.0, 7.0, 8.3};
+//        Double[] speedDivisionsArray = {2.0, 2.5, 3.2, 3.5, 4.0, 4.5, 5.0};
+//
+//        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
+//        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
+//
+//
+//        Double calorieTotal = calculateCaloriesFromStats(metValues, speedDivisions);
+//
+//        return calorieTotal;
+//        //if (this.getHeartRateList() == 0);
 //    }
 //
-//    double getConsumedCalories(String activityType) {
-//        return 0;
+//    public ArrayList<Double> getConsumedCaloriesBetweenPoints() {
+//
+//        Double[] metValuesArray = {2.8, 3.0, 3.5, 4.3, 5.0, 7.0, 8.3};
+//        Double[] speedDivisionsArray = {2.0, 2.5, 3.2, 3.5, 4.0, 4.5, 5.0};
+//
+//        ArrayList<Double> metValues = new ArrayList<Double>(Arrays.asList(metValuesArray));
+//        ArrayList<Double> speedDivisions = new ArrayList<Double>(Arrays.asList(speedDivisionsArray));
+//        ArrayList<Double> caloriesList = new ArrayList<>();
+//
+//        caloriesList = calculateCaloriesBurnedBetweenPointsFromStats(metValues, speedDivisions);
+//        return caloriesList;
 //    }
-//}
+}
+
