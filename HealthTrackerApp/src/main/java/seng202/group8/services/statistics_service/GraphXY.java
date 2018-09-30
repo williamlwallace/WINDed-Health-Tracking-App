cb0012d79426f112a9fef432e4f662b0b59e9a67
Clarke Mcfadzien <cmc280@uclive.ac.nz>
2018-09-30 19:26:13 +1300

    Added a new cass to statistics_service called TimeScale, and a new attribute called XAxisScale to GraphXY to fix the x axis ticks on the visualisations. A matching unit label was also implemented.
    Still need to fix x-axis for BMI and weight over time.
    
    Also need to make sure there is no bug in the way it is displayed.
    
    Also made 12PM display correctly as 12PM, rather than 12AM.

--- HealthTrackerApp/src/main/java/seng202/group8/services/statistics_service/GraphXY.java
+++ HealthTrackerApp/src/main/java/seng202/group8/services/statistics_service/GraphXY.java
@@ -10,6 +10,7 @@ public class GraphXY {
      */
     public ArrayList<Double> XAxis;
     public ArrayList<Double> YAxis;
+    public String XAxisScale;
 
 
     /**
@@ -22,6 +23,14 @@ public class GraphXY {
 
     public ArrayList<Double> getYAxis() {
         return this.YAxis;
+    }
+
+    /**
+     * Grab the units of the time scale of the x-axis.
+     * @return the units of the time scale of the x-axis (hours, minutes, or seconds) as a string.
+     */
+    public String getXAxisScale() {
+        return XAxisScale;
     }
 
     /**
@@ -41,6 +50,15 @@ public class GraphXY {
     }
 
     /**
+     * Sets the scale of the XAxis values.
+     * @param XAxisScale a string containing the units desired for the values on the x-axis (which are always time
+     *                   values).
+     */
+    public void setXAxisScale(String XAxisScale) {
+        this.XAxisScale = XAxisScale;
+    }
+
+    /**
      * Adds a string to the X Axis arrayList
      * @param value the data being added to the arrayList
      */
@@ -56,8 +74,11 @@ public class GraphXY {
         this.YAxis.add(value);
     }
 
+
     public GraphXY() {
         this.YAxis = new ArrayList<>();
         this.XAxis = new ArrayList<>();
+        // Default XAxisScale value.
+        this.XAxisScale = "(seconds)";
     }
 }
---
+++ HealthTrackerApp/src/main/java/seng202/group8/services/statistics_service/TimeScale.java
@@ -0,0 +1,31 @@
+package seng202.group8.services.statistics_service;
+
+import java.util.ArrayList;
+
+public class TimeScale {
+
+    private ArrayList<Double> times;
+    private String scale;
+
+    public TimeScale(ArrayList<Double> newTimes, String newScale) {
+        this.times = new ArrayList<>(newTimes);
+        this.scale = newScale;
+
+    }
+
+    public ArrayList<Double> getTimes() {
+        return times;
+    }
+
+    public void setTimes(ArrayList<Double> times) {
+        this.times = times;
+    }
+
+    public String getScale() {
+        return scale;
+    }
+
+    public void setScale(String scale) {
+        this.scale = scale;
+    }
+}
